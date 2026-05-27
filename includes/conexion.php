0b6df1c6d788918c6c2e1074861f2a1636995bc3
Dylan Rodas <dylanrodas10@gmail.com>
2026-05-27 15:54:52 -0600

    agregue estilo y componentes

---
+++ includes/conexion.php
@@ -0,0 +1,7 @@
+<?php
+$con = mysqli_connect("localhost","root","","votacion2028");
+
+if(!$con){
+    die("Error de conexión");
+}
+?>
---
+++ includes/sidebar.php
@@ -0,0 +1,13 @@
+<div class="sidebar">
+<h2>Votación 2028</h2>
+
+<a href="dashboard.php">Dashboard</a>
+<a href="usuarios.php">Usuarios</a>
+<a href="tipos_votacion.php">Tipos Votación</a>
+<a href="partidos.php">Partidos</a>
+<a href="candidatos.php">Candidatos</a>
+<a href="mesas.php">Mesas</a>
+<a href="resultados.php">Resultados</a>
+<a href="auditoria.php">Auditoría</a>
+<a href="../auth/logout.php">Cerrar sesión</a>
+</div>
